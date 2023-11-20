35f2fac0bc27ebb4ff0d1944daedc2aad09ff4b4
Byoungcheon Lee <lbcsultan@gmail.com>
2023-11-20 11:47:22 +0900

    1st

--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["gateway.ipfscdn.io"],
+    domains: [
+      'gateway.ipfscdn.io',
+      '5f8fb5f583ae1583336e978a2deaf9ed.ipfscdn.io',
+    ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
